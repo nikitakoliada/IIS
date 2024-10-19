1d044926ec8409acbb134b0285d34ba56ec47d18
MV <nikola@rootnix.net>
2024-10-19 19:34:55 +0200

    feat[controller]: user

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,51 @@
+using IIS.Data;
+using IIS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IIS.Repositories;
+
+public class UserRepository(ApplicationDbContext context)
+{
+    public Task<List<User>> GetAllAsync()
+    {
+        return context.Users.ToListAsync();
+    }
+    
+    public Task<List<User>> GetAllWithIncludesAsync()
+    {
+        return context.Users
+            .Include(u => u.AssignedStudio)
+            .ToListAsync();
+    }
+
+    public Task<User?> GetByIdAsync(string id)
+    {
+        return context.Users.FirstOrDefaultAsync(u => u.Id == id);
+    }
+    
+    public Task<bool> ExistsAsync(string id)
+    {
+        return context.Users.AnyAsync(u => u.Id == id);
+    }
+
+    public Task<int> CreateAsync(User user)
+    {
+        context.Add(user);
+
+        return context.SaveChangesAsync();
+    }
+
+    public Task<int> UpdateAsync(User user)
+    {
+        context.Update(user);
+
+        return context.SaveChangesAsync();
+    }
+
+    public Task<int> RemoveAsync(User user)
+    {
+        context.Remove(user);
+
+        return context.SaveChangesAsync();
+    }
+}
