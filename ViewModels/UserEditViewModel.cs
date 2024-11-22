b473ef610a27e48407b19dac9a9a0f2422017680
MV <nikola@rootnix.net>
2024-11-22 01:20:36 +0100

    feat: user controller

--- ViewModels/UserEditViewModel.cs
+++ ViewModels/UserEditViewModel.cs
@@ -7,8 +7,12 @@ public class UserEditViewModel
     public string Name { get; set; }
     public string Address { get; set; }
     public DateTime BirthDate { get; set; }
+    public int? AssignedStudioId { get; set; }
     
     public bool? IsAssignedToMyStudio { get; set; }
+    
+    public bool? IsAdmin { get; set; }
     public bool? IsStudioAdmin { get; set; }
     public bool? IsTeacher { get; set; }
+    public bool? IsStudent { get; set; }
 }
