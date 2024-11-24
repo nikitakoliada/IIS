1a00258d5de2421e207ddb830804fcf3c12b3580
koliadavutbr <xkolia00@vutbr.cz>
2024-11-24 20:50:11 +0100

    Added display names

--- Models/User.cs
+++ Models/User.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using Microsoft.AspNetCore.Identity;
 
 namespace IIS.Models;
@@ -6,9 +7,13 @@ public class User : IdentityUser
 {
     public required string Name { get; set; }
     public required string Address { get; set; }
+    
+    [DisplayName("Birth Date")]
     public required DateTime BirthDate { get; set; }
     
     public int? AssignedStudioId { get; set; }
+    
+    [DisplayName("Assigned Studio")]
     public Studio? AssignedStudio { get; set; }
 
     public ICollection<Borrow> BorrowedEquipment { get; set; } = new List<Borrow>();
